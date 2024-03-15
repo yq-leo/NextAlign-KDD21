787ea6b81404d1042dfc448b36cee8fbf0dcdce5
Leo Yu <qiyuleo@outlook.com>
2024-03-15 15:25:01 -0500

    update results

--- run_all.bat
+++ run_all.bat
@@ -1,11 +1,11 @@
 echo Running on Phone-Email dataset
-python train.py --epochs=5 --dataset=phone-email
+python train.py --epochs=200 --dataset=phone-email
 echo -----------------------------
 
 echo Running on Foursquare-Twitter dataset
-python train.py --epochs=5 --dataset=foursquare-twitter
+python train.py --epochs=200 --dataset=foursquare-twitter
 echo -----------------------------
 
 echo Running on ACM-DBLP dataset
-python train.py --epochs=1 --dataset=ACM-DBLP
+python train.py --epochs=200 --dataset=ACM-DBLP
 echo -----------------------------
