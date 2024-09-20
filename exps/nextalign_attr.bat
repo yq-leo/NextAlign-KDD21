0f73ee84804fa7848c8b04c76f8aac41b475141f
Leo Yu <qiyuleo@outlook.com>
2024-09-20 02:00:27 -0500

    bugfix

--- exps/nextalign_attr.bat
+++ exps/nextalign_attr.bat
@@ -1,9 +1,9 @@
-python main.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
+python train.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
 for %%N in (0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9) do (
-  python main.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=%%N
+  python train.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=%%N
 )
 
-python main.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
+python train.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
 for %%N in (0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9) do (
-  python main.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=%%N
+  python train.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=%%N
 )
--- exps/nextalign_attr.sh
+++ exps/nextalign_attr.sh
@@ -1,9 +1,9 @@
-#python main.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
+#python train.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
 #for attr_noise in $(seq 0.1 0.1 0.9); do
-#  python main.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=$attr_noise
+#  python train.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=$attr_noise
 #done
 
-python main.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
+python train.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10
 for attr_noise in $(seq 0.1 0.1 0.9); do
-  python main.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=$attr_noise
+  python train.py --dataset=Douban --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=$attr_noise
 done
