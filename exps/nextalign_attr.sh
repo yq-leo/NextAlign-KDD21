fe3cdb953619e47b785f20029af5dee69d7261f9
Qi Yu <qiyu6@idealab-01.cs.illinois.edu>
2024-09-23 02:47:52 -0500

    from server

--- exps/nextalign_attr.sh
+++ exps/nextalign_attr.sh
@@ -3,7 +3,7 @@
 #  python -u train.py --dataset=cora --use_attr --epochs=50 --record --exp_name=attr_noise --runs=10 --attr_noise=$attr_noise
 #done
 
-python -u train.py --dataset=Douban --use_attr --epochs=30 --record --exp_name=attr_noise --runs=10 --robust --strong_noise
+python -u train.py --dataset=Douban --use_attr --epochs=10 --record --exp_name=attr_noise --runs=10 --robust --strong_noise
 for attr_noise in $(seq 0.1 0.1 0.9); do
-  python -u train.py --dataset=Douban --use_attr --epochs=30 --record --exp_name=attr_noise --runs=10 --attr_noise=$attr_noise --robust --strong_noise
+  python -u train.py --dataset=Douban --use_attr --epochs=10 --record --exp_name=attr_noise --runs=10 --attr_noise=$attr_noise --robust --strong_noise
 done
